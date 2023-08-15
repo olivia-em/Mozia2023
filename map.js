659d7b1f82641b9741f4f97f9fa36821124729ca
Glitch (incantationbowl) <none>
2023-08-15 20:43:36 +0000

    📖🚓 Checkpoint ./index.html:48649160/13904 ./kiln.html:48649160/18645 ./style.css:48649160/14502 ./map.js:48649160/3317 ./.glitch-assets:48649160/4978 ./mozia.geojson:48649160/1239 ./writeup.html:48649160/2270 ./acknowledgments.html:48649160/2367 ./leaflet-providers.js:48649160/33286

--- map.js
+++ map.js
@@ -1,45 +1,88 @@
-let wtopo = L.tileLayer(
-  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
-  {
-    attribution:
-      "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community",
-  }
-);
-
-let map = L.map("map", {
-  layers: [wtopo],
-});
-
-// $.getJSON("assets/SlaveRevolts.geojson", function(data){
-
-//   let fourLayer = L.geoJson(data, {
-
-//   filter: function(feature, layer) {
-//     return (feature.properties["Century"] === "1400");},
-
-//   pointToLayer: function(feature, latlng) {
-//       return new L.CircleMarker(latlng, {
-//         radius:8,
-//         color: getColor(feature),
-//       });
-//     },
-//     onEachFeature: PointActions
-
-//   });
-
-//    fourLayer.addTo(map);
-
+// let wtopo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+//   attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 // });
 
-map.setView([37.867789995417226, 12.468310528245546], 15);
+let map = L.map('map', {
+    minZoom: 15,
+    maxZoom: 19
+});
 
-// let overlayMaps = {
-//     "1400s": fourLayer,
-//     "1500s": fiveLayer,
-//     "1600s": sixLayer,
-//     "1700s": sevenLayer,
-//     "1800s": eightLayer,
-//     "1900+": nineLayer
-// };
+var imageUrl = 'islandimage.png',
+    imageBounds = [[37.86188992,12.46104057], [37.87174639,12.47492509]];
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
-let layerControl = L.control.layers(baseMaps).addTo(map);
+$.getJSON("mozia.geojson", function(data){
+
+  function onEachFeature (feature, thing) {
+    thing.bindPopup('<div style="background-color: #f4eedc">'+'<div style="text-align: center; color: #030c8a; font-size: 9px"><b>'+ 
+      feature.properties["Name"] + '</b></div>' + '<div style="color: #030c8a; font-size: 8px">'+
+      feature.properties["Description"]+'</div></div>', {
+        maxWidth: 250
+      })
+  }
+
+  let scavi = L.geoJson(data, {
+
+  pointToLayer: function(feature, latlng) {
+      return new L.CircleMarker(latlng, {
+        radius:20,
+        color: '#c95410',
+           });
+    },
+    onEachFeature: onEachFeature
+    
+  });
+
+   scavi.addTo(map);
+
+});
+
+map.setView([37.8671797,12.4694909], setInitialMapZoom());
+
+var width = document.documentElement.clientWidth;
+
+window.addEventListener('resize', function(event){
+    // get the width of the screen after the resize event
+
+    // tablets are between 768 and 922 pixels wide
+    // phones are less than 768 pixels wide
+    if (width < 768) {
+        // set the zoom level to 15
+        map.setZoom(14);
+    } else if (width >= [768] && width < [922]) {
+
+   mapZoom = [15];
+
+}
+    else {
+        // set the zoom level to 16
+        map.setZoom(16);
+    }
+
+});
+
+function setInitialMapZoom() {
+
+  let viewportWidth = window.innerWidth;
+
+  let mapZoom;
+
+  if (viewportWidth < [768]) {
+
+   mapZoom = [14];
+
+}  else if (viewportWidth >= [768] && viewportWidth < [922]) {
+
+   mapZoom = [15];
+
+}  else {
+
+   mapZoom = [16];
+
+}
+
+return mapZoom;
+
+}
+
+
