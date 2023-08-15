2a6121279c31cc1bb9f852f4b7dc3e0dec909f99
Glitch (incantationbowl) <none>
2023-08-15 20:55:13 +0000

    🏅🌆 Checkpoint ./.glitch-assets:48649160/346 ./kiln.html:48649160/26061 ./index.html:48649160/497 ./map.js:48649160/8730 ./leaflet-providers.js:48649160/72285 ./acknowledgments.html:48649160/5180 ./style.css:48649160/16191 ./writeup.html:48649160/4801

--- map.js
+++ map.js
@@ -2,87 +2,80 @@
 //   attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 // });
 
-let map = L.map('map', {
-    minZoom: 15,
-    maxZoom: 19
+let map = L.map("map", {
+  minZoom: 15,
+  maxZoom: 19,
 });
 
-var imageUrl = 'islandimage.png',
-    imageBounds = [[37.86188992,12.46104057], [37.87174639,12.47492509]];
+var imageUrl =
+    "https://cdn.glitch.global/f39f54fe-e827-46f0-afb0-3c4066c9677c/islandimage.png?v=1692131807579",
+  imageBounds = [
+    [37.86188992, 12.46104057],
+    [37.87174639, 12.47492509],
+  ];
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
-$.getJSON("mozia.geojson", function(data){
-
-  function onEachFeature (feature, thing) {
-    thing.bindPopup('<div style="background-color: #f4eedc">'+'<div style="text-align: center; color: #030c8a; font-size: 9px"><b>'+ 
-      feature.properties["Name"] + '</b></div>' + '<div style="color: #030c8a; font-size: 8px">'+
-      feature.properties["Description"]+'</div></div>', {
-        maxWidth: 250
-      })
+$.getJSON("mozia.geojson", function (data) {
+  function onEachFeature(feature, thing) {
+    thing.bindPopup(
+      '<div style="background-color: #f4eedc">' +
+        '<div style="text-align: center; color: #030c8a; font-size: 9px"><b>' +
+        feature.properties["Name"] +
+        "</b></div>" +
+        '<div style="color: #030c8a; font-size: 8px">' +
+        feature.properties["Description"] +
+        "</div></div>",
+      {
+        maxWidth: 250,
+      }
+    );
   }
 
   let scavi = L.geoJson(data, {
-
-  pointToLayer: function(feature, latlng) {
+    pointToLayer: function (feature, latlng) {
       return new L.CircleMarker(latlng, {
-        radius:20,
-        color: '#c95410',
-           });
+        radius: 20,
+        color: "#c95410",
+      });
     },
-    onEachFeature: onEachFeature
-    
+    onEachFeature: onEachFeature,
   });
 
-   scavi.addTo(map);
-
+  scavi.addTo(map);
 });
 
-map.setView([37.8671797,12.4694909], setInitialMapZoom());
+map.setView([37.8671797, 12.4694909], setInitialMapZoom());
 
 var width = document.documentElement.clientWidth;
 
-window.addEventListener('resize', function(event){
-    // get the width of the screen after the resize event
+window.addEventListener("resize", function (event) {
+  // get the width of the screen after the resize event
 
-    // tablets are between 768 and 922 pixels wide
-    // phones are less than 768 pixels wide
-    if (width < 768) {
-        // set the zoom level to 15
-        map.setZoom(14);
-    } else if (width >= [768] && width < [922]) {
-
-   mapZoom = [15];
-
-}
-    else {
-        // set the zoom level to 16
-        map.setZoom(16);
-    }
-
+  // tablets are between 768 and 922 pixels wide
+  // phones are less than 768 pixels wide
+  if (width < 768) {
+    // set the zoom level to 15
+    map.setZoom(14);
+  } else if (width >= [768] && width < [922]) {
+    mapZoom = [15];
+  } else {
+    // set the zoom level to 16
+    map.setZoom(16);
+  }
 });
 
 function setInitialMapZoom() {
-
   let viewportWidth = window.innerWidth;
 
   let mapZoom;
 
   if (viewportWidth < [768]) {
+    mapZoom = [14];
+  } else if (viewportWidth >= [768] && viewportWidth < [922]) {
+    mapZoom = [15];
+  } else {
+    mapZoom = [16];
+  }
 
-   mapZoom = [14];
-
-}  else if (viewportWidth >= [768] && viewportWidth < [922]) {
-
-   mapZoom = [15];
-
-}  else {
-
-   mapZoom = [16];
-
+  return mapZoom;
 }
-
-return mapZoom;
-
-}
-
-
